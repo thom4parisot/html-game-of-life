3a93b624829a406c4984e53127da8b3f4b7bba10
oncletom <thomas@oncle-tom.net>
2011-12-03 16:20:22 +0100

    Result of the test

---
+++ lib/casper/README.md
@@ -0,0 +1,17 @@
+# Casper.js
+
+Casper.js is a navigation scripting & testing utility for [PhantomJS](http://www.phantomjs.org/). It eases the process of defining a full navigation scenario and provides useful high-level functions, methods & syntaxic sugar for doing common tasks such as:
+
+- defining & ordering navigation steps
+- filling forms
+- clicking links
+- capturing screenshots of a page (or an area)
+- making assertions on remote DOM
+- logging events
+- downloading base64 encoded resources, even binary ones
+- catching errors and react accordingly
+- writing functional test suite, exporting results as JUnit XML (xUnit)
+
+Feel free to browse our [sample examples repository](https://github.com/n1k0/casperjs/tree/master/samples). Don't hesitate to pull request for any cool example of yours as well!
+
+**Read the [full documentation](http://n1k0.github.com/casperjs/) on casperjs dedicated website.**
---
+++ tests/grid_test.js
@@ -0,0 +1,34 @@
+phantom.injectJs("../lib/casper/casper.js");
+
+var casper = new phantom.Casper();
+
+casper.start("grid.html", function(self){
+	self.test.assertEval(function(){
+		return document.querySelector("#grid") ? true : false;
+	}, "Expecting a table with id as 'grid'.");
+});
+
+casper.then(function(self){
+	self.test.assertEvalEquals(function(){
+		return document.querySelectorAll("#grid td").length;
+	}, 100, "Expecting 100 cells");
+});
+
+casper.then(function(self){
+	self.test.assertEvalEquals(function(){
+		return document.querySelectorAll("#grid td.alive").length;
+	}, 4, "Expecting 4 living cells");
+});
+
+casper.then(function(self){
+	self.evaluate(function(){
+		self.echo(Grid.isCellAlive(document.querySelector("#grid tr:nth-child(2) td:nth-child(4)")) ? 1 : 0);
+	});
+	self.test.assertEval(function(){
+		return Grid.isCellAlive(document.querySelector("#grid tr:nth-child(2) td:nth-child(3)"));
+	}, "Expecting Living cell");
+});
+
+casper.run(function(self){
+	self.exit();
+});
