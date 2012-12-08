4341c979fd671fcfcb404be0b92f68e974c96a93
oncletom <thomas@oncle-tom.net>
2012-12-08 21:53:29 +0100

    Compatibility with Casper 1.0

--- tests/grid_test.js
+++ tests/grid_test.js
@@ -1,29 +1,30 @@
-phantom.injectJs("../lib/casperjs/casper.js");
-
-var casper = new phantom.Casper({loglevel: "debug", verbose: true});
-
-casper.start("grid.html", function(self){
-	self.test.assertExists("#grid", "Expecting a table with id as 'grid'.");
+var casper = require('casper').create({
+	loglevel: "debug",
+	verbose: true
 });
 
-casper.then(function(self){
-	self.test.assertEvalEquals(function(){
+casper.start("grid.html", function(){
+	this.test.assertExists("#grid", "Expecting a table with id as 'grid'.");
+});
+
+casper.then(function(){
+	this.test.assertEvalEquals(function(){
 		return document.querySelectorAll("#grid td").length;
 	}, 100, "Expecting 100 cells");
 });
 
-casper.then(function(self){
-	self.test.assertEvalEquals(function(){
+casper.then(function(){
+	this.test.assertEvalEquals(function(){
 		return document.querySelectorAll("#grid td.alive").length;
 	}, 4, "Expecting 4 living cells");
 });
 
-casper.then(function(self){
-	self.test.assertEval(function(){
+casper.then(function(){
+	this.test.assertEval(function(){
 		return GridInstance.isCellAlive(document.querySelector("#grid tr:nth-child(2) td:nth-child(4)")) === true;
 	}, "Expecting Living cell");
 
-	self.test.assertEval(function(){
+	this.test.assertEval(function(){
 		return GridInstance.isCellAlive(document.querySelector("#grid tr:nth-child(2) td:nth-child(3)")) === false;
 	}, "Expecting Dead cell");
 });
@@ -31,14 +32,14 @@ casper.then(function(self){
 /*
  * Adjacent cells tests
  */
-casper.then(function(self){
-	self.echo("Working on cell (2,4) which is alive.", "COMMENT");
+casper.then(function(){
+	this.echo("Working on cell (2,4) which is alive.", "COMMENT");
 
-	self.test.assertEvalEquals(function(){
+	this.test.assertEvalEquals(function(){
 		return GridInstance.getAdjacents(document.querySelector("#grid tr:nth-child(2) td:nth-child(4)")).length;
 	}, 8, "Expecting 8 adjacent cells");
 
-	self.test.assertEvalEquals(function(){
+	this.test.assertEvalEquals(function(){
 		return GridInstance.getAdjacents(document.querySelector("#grid tr:nth-child(2) td:nth-child(4)"), GridInstance.isCellAlive).length;
 	}, 2, "Expecting 2 alive adjacent cells");
 });
@@ -46,26 +47,26 @@ casper.then(function(self){
 /*
  * Working on next state
  */
- casper.then(function(self){
-	self.echo("Next Grid State.", "COMMENT");
+ casper.then(function(){
+	this.echo("Next Grid State.", "COMMENT");
 
-	self.evaluate(function(){
+	this.evaluate(function(){
 		GridInstance.nextState();
 	});
 
-	self.test.assertEvalEquals(function(){
+	this.test.assertEvalEquals(function(){
 		return GridInstance.getAdjacents(document.querySelector("#grid tr:nth-child(2) td:nth-child(4)"), GridInstance.isCellAlive).length;
 	}, 3, "Expecting now 3 alive adjacent cells");
 
-	self.test.assertEval(function(){
+	this.test.assertEval(function(){
 		return GridInstance.isCellAlive(document.querySelector("#grid tr:nth-child(3) td:nth-child(5)")) === true;
 	}, "Next diagonal cell is now alive.");
 
-	self.test.assertEval(function(){
+	this.test.assertEval(function(){
 		return GridInstance.isCellAlive(document.querySelector("#grid tr:nth-child(9) td:nth-child(6)")) === false;
 	}, "Alone cell should have died.");
 });
 
 casper.run(function(self){
-	self.exit();
+	this.test.renderResults(true);
 });
